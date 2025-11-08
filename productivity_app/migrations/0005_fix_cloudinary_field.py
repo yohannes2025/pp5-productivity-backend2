20879c2f46e4961f4ca2a671486e93df0d18ce8d
yohannes2025 <johannes.m.tekle@gmail.com>
2025-11-08 17:58:23 +0100

    Fix CloudinaryField migration

---
+++ productivity_app/migrations/0005_fix_cloudinary_field.py
@@ -0,0 +1,23 @@
+# productivity_app/migrations/0005_fix_cloudinary_field.py
+from django.db import migrations
+import cloudinary.models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        # <-- change only if your last migration number is different
+        ('productivity_app', '0004_file'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='file',
+            name='file',
+            field=cloudinary.models.CloudinaryField(
+                max_length=255,
+                verbose_name='file',
+                folder='task_files'
+            ),
+        ),
+    ]
